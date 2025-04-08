827ab895f1ba9191bbaeecf51860a47ebeae87e0
Vhens Heinrich D. Cayas <vdcayas.student@asiancollege.edu.ph>
2025-04-08 11:36:13 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route("/home", methods=["POST"])
-def home():
-    return render_template('contact_form.html')
+@app.route('/')
+def student():
+   return render_template('contact_form.html')
 
-if __name__ == "__main__":
-    app.run(debug == True)
+@app.route('/result',methods = ['POST', 'GET'])
+def result():
+   if request.method == 'POST':
+      result = request.form
+      return render_template("result.html",result = result)
+
+if __name__ == '__main__':
+   app.run(debug = True)
