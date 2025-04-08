c3fd03ee6c2466b02d862aa6a83fa8fc269e1f8b
Vhens Heinrich D. Cayas <vdcayas.student@asiancollege.edu.ph>
2025-04-08 10:48:40 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.rout("/", methods=["GET"])
+@app.route("/", methods=["GET"])
 def home():
     return "<h1>Vhens Flask Website</h1>"
 
 if __name__ == "__main__":
-    app.run
+    app.run()
